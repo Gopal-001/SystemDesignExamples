a5ff162965fa19ebfc0adbe74f7a3b1f9de821b9
_sameer <sameer.mishra@infra.market>
2024-09-24 22:19:09 +0530

    Added MiniDuckSimulator Code

---
+++ Ch1/MiniDuckSimulator.java
@@ -0,0 +1,12 @@
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+        // Model duck with dynamic adding fly behavior
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+    }
+}
